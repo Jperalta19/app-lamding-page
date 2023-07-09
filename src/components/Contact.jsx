73c9f1211ef579404aaf1239d4d34b3ae571416e
Your Name <you@example.com>
2023-07-09 13:06:37 -0400

    .

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Contact() {
   return (
     <div id='contact'>
-        <h3>Send Me Mail</h3>
+        <h3>Send Me A Mail</h3>
         <div className='contact-input'>
             <input type='email' placeholder='Example@gmail.com'/>
             <a href='https://www.instagram.com/Jperalta_'>Contact</a>
